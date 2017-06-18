901d3400cf0027794f8c7e650fe54234b7361e1d
Rob <cowboysfn007@gmail.com>
2017-06-18 15:58:23 -0700

    firs tcommit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.post('/standings', function (req, res) {
+  res.send(response)
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
+var response = {
+"speech": "1. Rob Miller, 2. Jeff Hebert",
+"displayText": "1. Rob Miller, 2. Jeff Hebert... ",
+"source": "JeffBot"
+}
