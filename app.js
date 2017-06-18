114266604197a7273e22aed0a1de5e52934c1170
Rob <cowboysfn007@gmail.com>
2017-06-18 16:10:41 -0700

    added back get

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
 app.post('/standings', function (req, res) {
   res.send(response)
 })
